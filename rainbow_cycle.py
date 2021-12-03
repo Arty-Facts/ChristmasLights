72489b0a5b044601bca870b5cf48d94c57154518
Arturas Aleksandrauskas <arturas.aleksandrauskas@contextvision.se>
2021-12-03 13:13:39 +0100

    t

--- rainbow_cycle.py
+++ rainbow_cycle.py
@@ -7,8 +7,9 @@ import neopixel
 pixel_pin = board.D18
 num_pixels = 100
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=True)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False)
 pixels.fill((0,0,0))
+pixels.show()
 
 def color_chase(color, wait):
     for i in range(num_pixels):
@@ -16,7 +17,6 @@ def color_chase(color, wait):
         time.sleep(wait)
         pixels.show()
     time.sleep(0.5)
-
 
 def rainbow_cycle(wait):
     for j in range(255):
@@ -27,23 +27,23 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
+RED = (0, 255, 0)
+YELLOW = (150, 255, 0)
+GREEN = (255, 0, 0)
+CYAN = (255, 0, 255)
 BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-# print("Running...")
-# i = 1
-# for c in [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]:
-#     print(f"fill {c}")
-#     color_chase(c, 0.5)
-#     pixels.show()
-#     # Increase or decrease to change the speed of the solid color change.
-#     time.sleep(5)
+PURPLE = (0, 180, 255)
+print("Running...")
+i = 1
+for c in [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]:
+    print(f"fill {c}")
+    color_chase(c, 0.5)
+    pixels.show()
+    # Increase or decrease to change the speed of the solid color change.
+    time.sleep(5)
 
-# print("while")
-# while True:
-#     rainbow_cycle(1)  # Increase the number to slow down the rainbow
-#     print(f"cycle {i}", end="\r")
-#     i += 1
+print("while")
+while True:
+    rainbow_cycle(1)  # Increase the number to slow down the rainbow
+    print(f"cycle {i}", end="\r")
+    i += 1
