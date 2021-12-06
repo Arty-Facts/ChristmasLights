4b30945bdcfeddd19f598f80a2d7ca90ce95c3cf
Arturas Aleksandrauskas <artal938@student.liu.se>
2021-12-06 20:15:52 +0100

    simple test

---
+++ simpleTest.py
@@ -0,0 +1,12 @@
+import board
+import neopixel
+from time import sleep
+
+pixel_pin = board.D18
+num_pixels = 8
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5)
+pixels.fill((0,0,0))
+sleep(2)
+pixels.fill((255,0,0))
+pixels.show()
