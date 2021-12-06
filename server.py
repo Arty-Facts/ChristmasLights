7628b92ddf041ff8f76f74ffc94357b456d7c843
Arturas Aleksandrauskas <artal938@student.liu.se>
2021-12-06 18:48:15 +0100

    server done

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+from lights import Lights
 
 app = Flask(__name__)
+pixels = Lights()
+num_pixels = len(pixels)
 
 @app.route("/")
 def home():
@@ -8,11 +11,16 @@ def home():
 
 @app.route("/clear", methods=['GET', "POST"])
 def clear():
+    pixels.clear_all()
     return "All clear"
 
 
 @app.route('/set', methods=['POST'])
 def set():
+    for index, rgb in request.form.items():
+        r, g, b = list(map(int, rgb.split(",")))
+        pixels[int(index)] = (r, g, b)
+    pixels.show()
     return "\n".join((f"{k}->{v}" for k,v in request.form.items()))
 
 if __name__ == '__main__':
